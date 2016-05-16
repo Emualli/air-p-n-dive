29fdb9bfd35447fe024f2426ef6c51ca539a5fe6
Arthur Vinson <arthur.vinson@polytechnique.org>
2016-05-16 17:36:01 +0200

    V0 views et controlleur

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,42 @@
 class BookingsController < ApplicationController
+  before_action :show, :edit, :update
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(params[:booking])
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:status, :start_date, :end_date, :price)
+  end
+
 end
