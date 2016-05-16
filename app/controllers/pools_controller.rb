a9c307079786ac7bb74bb64ac18037328b62d8b1
Jean-Philippe Roy <jp.roy.84@gmail.com>
2016-05-16 17:34:18 +0200

    added actions + views

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -1,2 +1,44 @@
 class PoolsController < ApplicationController
+  before_action :find_pool, only: [:show, :edit, :update]
+
+  def index
+    @pools = Pool.all
+  end
+
+  def show
+    find_pool
+  end
+
+  def edit
+    find_pool
+  end
+
+  def update
+    find_pool
+    @restaurant.update(params[:restaurant])
+  end
+
+  def new
+    @pool = Pool.new
+    @pool.user = current_user
+  end
+
+  def create
+    @pool = Pool.new(pool_params)
+    if @pool.save
+      redirect_to pool_path(@pool)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_pool
+    @pool = pool.find(params[:id])
+  end
+
+  def pool_params
+    params.require(:pool).permit(:user_id, :width, :length, :depth, :shape, :location, :water_type, :latitude, :longitude, :equipments, :price, :description, :status)
+  end
 end
