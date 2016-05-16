56343a4eafd3ece959f20e64f6c661f0611173a9
Guillaume Degermann <guillaume.deger@gmail.com>
2016-05-16 16:42:21 +0200

    Added Devise + DB migration + devise views

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -1,4 +1,5 @@
 class Pool < ActiveRecord::Base
   belongs_to :user
-  has_many :agendas, :bookings
+  has_many :agendas
+  has_many :bookings
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'home#index'
   resources :pools, except: [:destroy]
   resources :users, except: [:destroy]
   resources :bookings, except: [:destroy]
