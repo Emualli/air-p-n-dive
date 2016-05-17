27b0626d4cd70e2abc14b25e0f9bbda9da029061
Guillaume Degermann <guillaume.deger@gmail.com>
2016-05-17 18:40:31 +0200

    Add layout & logic behind booking interface

--- app/assets/javascripts/bookings.js
+++ app/assets/javascripts/bookings.js
@@ -1,2 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+  $('.tab').on('click', function() {
+    $('.tab').removeClass('active');
+    $(this).addClass('active');
+  });
+  $('#customer').on('click', function() {
+    $('#display_customer').addClass('active');
+    $('#display_owner').removeClass('active');
+  });
+  $('#owner').on('click', function() {
+    $('#display_customer').removeClass('active');
+    $('#display_owner').addClass('active');
+  });
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,15 @@ class UsersController < ApplicationController
   end
 
   def bookings
-    @bookings = Booking.where("user_id = #{current_user.id}")
+    # Get Customer bookings for the user :
+    @customer_bookings = Booking.where("user_id = #{current_user.id}")
+
+    # Get Owner reservations for the user :
+    @owned_pools = Pool.where("user_id = #{current_user.id}")
+    @owner_bookings = []
+    @owned_pools.each do |pool|
+      @owner_bookings << Booking.find_by("pool_id =  ?", pool.id) if !Booking.find_by("pool_id =  ?", pool.id).nil?
+    end
   end
 
   def pools
