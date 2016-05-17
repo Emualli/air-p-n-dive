dfa979c0496feb7316c0f44b714b83de6b510951
Arthur Vinson <arthur.vinson@polytechnique.org>
2016-05-17 12:40:18 +0200

    added filter by location and date(not tested)

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -2,7 +2,13 @@ class PoolsController < ApplicationController
   before_action :find_pool, only: [:show, :edit, :update]
 
   def index
-    @pools = Pool.all
+    @location = params[:location].downcase
+    @pools = search_pools_by_location
+    if params.has_key?(:reservation_date)
+      reservation_string = params[:reservation_date].gsub!('%2F','/')
+      @reservartion_date = date_string_to_date(reservation_string)
+      @pools = search_pools_by_availability
+    end
   end
 
   def show
@@ -43,4 +49,24 @@ class PoolsController < ApplicationController
   def pool_params
     params.require(:pool).permit(:user_id, :width, :length, :depth, :shape, :location, :water_type, :latitude, :longitude, :equipments, :price, :description, :status)
   end
+
+  def search_pools_by_location
+    return Pool.where("location = ?", @location.capitalize)
+  end
+
+  def search_pool_by_availability
+    results = []
+    @pools.each do |pool|
+      pool.agendas.each do |agenda|
+        results << pool if ( @reservation_date >= date_string_to_date(agenda.start_date) &&  @reservation_date <= date_string_to_date(agenda.end_date))
+      end
+    end
+    return results
+  end
+
+  def date_string_to_date(date_string)
+    date_array = date_string.split('/')
+    return Date.new(date_array[2], date_array[1], date_array[0])
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
 Pool.create(description: 'nice pool', location: 'Paris', width: 3, user_id: 1)
 Pool.create(description: 'very nice pool', location: 'London', width: 3, user_id: 1)
-Pool.create(description: 'blue pool', location: 'Marrakech', width: 3, user_id: 1)
+Pool.create(description: 'blue pool', location: 'London', width: 3, user_id: 1)
 Pool.create(description: 'green pool', location: 'Amsterdam', width: 3, user_id: 1)
+Pool.create(description: 'purple pool', location: 'Amsterdam', width: 3, user_id: 1)
+Pool.create(description: 'red pool', location: 'Amsterdam', width: 3, user_id: 1)
