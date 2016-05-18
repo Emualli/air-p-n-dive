02de9400107b59c05105a22bfb087a70a64fb104
Guillaume Degermann <guillaume.deger@gmail.com>
2016-05-18 13:28:14 +0200

    Change maps http request to http instead of https

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,6 @@
 Geocoder.configure(
   lookup:    :google,
   api_key:   ENV['GOOGLE_MAPS_SERVER_KEY'],
-  use_https: true,
+  use_https: false,
   units:     :km       # :km for kilometers or :mi for mile
 )
