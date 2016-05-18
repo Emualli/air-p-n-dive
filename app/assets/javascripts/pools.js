968b854d4c253b54d986a87147078c2aabdbe51f
Guillaume Degermann <guillaume.deger@gmail.com>
2016-05-18 10:52:49 +0200

    add datetimepicker

--- app/assets/javascripts/pools.js
+++ app/assets/javascripts/pools.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+  $('#booking_start_time').datetimepicker({
+    format: "HH:mm"
+  });
+  $('#booking_end_time').datetimepicker({
+    format: "HH:mm"
+  });
+});
