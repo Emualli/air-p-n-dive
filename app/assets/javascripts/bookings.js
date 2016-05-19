638fbb1059e2f57db73a742c1d0972ea1ccb352f
Guillaume Degermann <guillaume.deger@gmail.com>
2016-05-19 14:57:27 +0200

    Working datepicker :)

--- app/assets/javascripts/bookings.js
+++ app/assets/javascripts/bookings.js
@@ -1,23 +1,4 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).ready(function(){
-  $('#booking_date').datepicker({
-    format: 'dd/mm/yyyy',
-    startDate: '-3d',
-    datesDisabled: ['15/05/2016', '25/05/2016']
-  });
 
-  $('.tab').on('click', function() {
-    $('.tab').removeClass('active');
-    $(this).addClass('active');
-  });
-  $('#customer').on('click', function() {
-    $('#display_customer').addClass('active');
-    $('#display_owner').removeClass('active');
-  });
-  $('#owner').on('click', function() {
-    $('#display_customer').removeClass('active');
-    $('#display_owner').addClass('active');
-  });
-});
